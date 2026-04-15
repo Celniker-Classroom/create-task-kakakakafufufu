964c4414c946fc8d5387d1aeba9d8c48b154909b
kakakakafufufu <chuir616@gmail.com>
2026-04-15 05:34:34 +0000

    Finish the recording data part at first, now working on updating the most popular option and vote percent part. The data part uses two lists: one is used for storage the time the option is select, another one checks the option if it's selected or not

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-// add javascript here
+let vote = [0, 0, 0, 0]
+document.getElementById("submit").addEventListener("click", function(){submitVote()})
+    
+function submitVote(){
+let checkboxes = [
+document.getElementById("op1"),
+document.getElementById("op2"),
+document.getElementById("op3"),
+document.getElementById("op4")
+]
+for(let i =0;i < checkboxes.length;i++){
+    if(checkboxes[i].checked){
+       vote[i]++ 
+    }
+}
+    updateVote()
+}
+function updateVote(){
+    let popularest = 0
+    let term = 0
+    for(let i =0;i < vote.length;i++){
+        if(vote[i] > popularest){
+             popularest = vote[i]
+             term = i
+        }
+        document.getElementById("totalVotes").textContent = vote[0]+" "+vote[1]+" "+vote[2]+" "+vote[3]
+        + "popularest: " +vote[term]+"  total votes:"+popularest
+}
+}
