c781c6208b08b69e58458bc5f1c5cc09d4879420
kakakakafufufu <chuir616@gmail.com>
2026-04-16 05:55:21 +0000

    Finish the part about showing normal information about the votes and now I'm working on the vote percent and learning how to make a list text on html by js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let vote = [0, 0, 0, 0]
+let totalVotes = 0
 document.getElementById("submit").addEventListener("click", function(){submitVote()})
     
 function submitVote(){
@@ -18,12 +19,33 @@ for(let i =0;i < checkboxes.length;i++){
 function updateVote(){
     let popularest = 0
     let term = 0
+    totalVotes = 0
     for(let i =0;i < vote.length;i++){
         if(vote[i] > popularest){
-             popularest = vote[i]
-             term = i
+            popularest = vote[i]
+            term = i + 1
         }
+        
+        for(let i =0;i < vote.length;i++){
+            totalVotes += vote[i]
+        }
+        calculatePercent()
         document.getElementById("totalVotes").textContent = vote[0]+" "+vote[1]+" "+vote[2]+" "+vote[3]
-        + "popularest: " +vote[term]+"  total votes:"+popularest
+        + "popularest: " +popularest+"  total votes:"+ totalVotes
+        document.getElementById("mostVote").textContent = "The most popular thing is: "+term
 }
+}
+function calculatePercent(){
+    let percentList = document.getElementById("result")
+    let percentResult = document.createElement("li")
+    let calculateList = []
+    for(let i=0;i< vote.length; i++){
+        calculateList.push(
+            (vote[i]/totalVotes *100).toFixed(2)
+        )
+    }
+    for(let i=0;i< calculateList.length; i++){
+    percentResult.textContent = calculateList[i]+"%"
+    percentList.appendChild(percentResult)
+    }
 }
