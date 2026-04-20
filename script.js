55f6915a77e2bc2198980de298619152213ebd37
kakakakafufufu <chuir616@gmail.com>
2026-04-20 17:34:51 +0000

    Real final version, I find that the procedure doesn't fit the ilteration so I change the function from "showResult" in to "findMax" because this part of codes fit the conditions better

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let vote = [0, 0, 0, 0]
 let totalVotes = 0
 let max = 0
 document.getElementById("submit").addEventListener("click", function(){
+    let options = ["cat","dog","snake","hamster"]
     document.getElementById("remind").textContent = "Choose all the pets you like:"
     let checkboxes = [
     document.getElementById("op1"),
@@ -16,19 +17,26 @@ document.getElementById("submit").addEventListener("click", function(){
         }
     }
 
-    let popularest = 0
     totalVotes = 0
-    max = 0
     for(let i =0;i < vote.length;i++){
-        if(vote[i] > popularest){
-            popularest = vote[i]
-            max = i + 1
-        }
-        for(let i =0;i < vote.length;i++){
             totalVotes += vote[i]
         }
+
+    max = findMax(vote)
+
+        if (totalVotes != 0){
+        let percentList = [0,0,0,0]
+        for(let i=0;i < vote.length; i++){
+        percentList[i] = (
+        (vote[i]/totalVotes *100).toFixed(2)
+        )
     }
-        showResult(vote,totalVotes)
+    for(let i=0;i < percentList.length; i++){
+    document.getElementById('percent'+(i+1)).textContent = options[i] +": "+vote[i]+" votes"+" / "+percentList[i]+"%"
+    }
+        document.getElementById("totalVotes").textContent = "Total Votes: "+ totalVotes
+        document.getElementById("mostVote").textContent = "The most popular thing is: "+ options[max-1]
+        }
     })
 
 document.getElementById("clearBtn").addEventListener("click", function(){
@@ -43,30 +51,23 @@ document.getElementById("clearBtn").addEventListener("click", function(){
     vote = [0, 0, 0, 0]
     totalVotes = 0
     max = 0
-    showResult(vote,totalVotes)
-})
-
-function showResult(List,sum){
-    let options = ["cat","dog","snake","hamster"]
-    if (sum != 0){
-        let percentList = [0,0,0,0]
-        for(let i=0;i < List.length; i++){
-        percentList[i] = (
-        (List[i]/sum *100).toFixed(2)
-        )
-    }
-    for(let i=0;i < percentList.length; i++){
-    document.getElementById('percent'+(i+1)).textContent = options[i] +": "+List[i]+" votes"+" / "+percentList[i]+"%"
-    }
-        document.getElementById("totalVotes").textContent = "Total Votes: "+ sum
-        document.getElementById("mostVote").textContent = "The most popular thing is: "+ options[max-1]
-        }else{
-        document.getElementById("percent1").textContent = "Cat: --"
+    document.getElementById("percent1").textContent = "Cat: --"
         document.getElementById("percent2").textContent = "Dog: --"
         document.getElementById("percent3").textContent = "Snake: --"
         document.getElementById("percent4").textContent = "Hamster: --"
         document.getElementById("totalVotes").textContent = "Total Votes: "
         document.getElementById("mostVote").textContent = "The most popular thing is: "
+})
+
+function findMax(voteList){
+    let popularest = 0
+    let max = 0
+    for(let i =0;i < voteList.length;i++){
+        if(voteList[i] > popularest){
+            popularest = voteList[i]
+            max = i + 1
         }
+    }
+    return max;
 }
 
