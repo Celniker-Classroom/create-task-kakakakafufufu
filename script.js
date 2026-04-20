568934c87cb4361b9dd74c8413f01d3fff204d29
kakakakafufufu <chuir616@gmail.com>
2026-04-20 15:48:57 +0000

    Replace "Options" with the real things I want to survey: what pet do people like more

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let vote = [0, 0, 0, 0]
 let totalVotes = 0
 let max = 0
 document.getElementById("submit").addEventListener("click", function(){
-    document.getElementById("remind").textContent = "Choose all the things you like:"
+    document.getElementById("remind").textContent = "Choose all the pets you like:"
     let checkboxes = [
     document.getElementById("op1"),
     document.getElementById("op2"),
@@ -48,6 +48,7 @@ document.getElementById("clearBtn").addEventListener("click", function(){
 })
 
 function showResult(List,sum){
+    let options = ["cat","dog","snake","hamster"]
     if (sum != 0){
         let percentList = [0,0,0,0]
         for(let i=0;i < List.length; i++){
@@ -56,15 +57,15 @@ function showResult(List,sum){
         )
     }
     for(let i=0;i < percentList.length; i++){
-    document.getElementById('percent'+(i+1)).textContent = "Option "+ (i+1) +": "+List[i]+" votes"+" / "+percentList[i]+"%"
+    document.getElementById('percent'+(i+1)).textContent = options[i] +": "+List[i]+" votes"+" / "+percentList[i]+"%"
     }
         document.getElementById("totalVotes").textContent = "Total Votes: "+ sum
-        document.getElementById("mostVote").textContent = "The most popular thing is: "+ max
+        document.getElementById("mostVote").textContent = "The most popular thing is: "+ options[max-1]
         }else{
-        document.getElementById("percent1").textContent = "Option 1: --"
-        document.getElementById("percent2").textContent = "Option 2: --"
-        document.getElementById("percent3").textContent = "Option 3: --"
-        document.getElementById("percent4").textContent = "Option 4: --"
+        document.getElementById("percent1").textContent = "Cat: --"
+        document.getElementById("percent2").textContent = "Dog: --"
+        document.getElementById("percent3").textContent = "Snake: --"
+        document.getElementById("percent4").textContent = "Hamster: --"
         document.getElementById("totalVotes").textContent = "Total Votes: "
         document.getElementById("mostVote").textContent = "The most popular thing is: "
         }
