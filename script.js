a0bab01a7826562e863fcec51a0cfe8e3d14a8a7
kakakakafufufu <chuir616@gmail.com>
2026-04-20 09:27:55 +0000

    Final version after checking all the things and include all the things

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
+//used ap to help debug the code
 let vote = [0, 0, 0, 0]
 let totalVotes = 0
-document.getElementById("submit").addEventListener("click", function(){submitVote()})
-document.getElementById("clearBtn").addEventListener("click", function(){clear()})
-function submitVote(){
+let max = 0
+document.getElementById("submit").addEventListener("click", function(){
+    document.getElementById("remind").textContent = "Choose all the things you like:"
     let checkboxes = [
     document.getElementById("op1"),
     document.getElementById("op2"),
@@ -14,61 +15,55 @@ function submitVote(){
         vote[i]++ 
         }
     }
-    updateVote()
-}
-function updateVote(){
+
     let popularest = 0
-    let unpopularest = 500
-    let max = 0
-    let min = 0
     totalVotes = 0
+    max = 0
     for(let i =0;i < vote.length;i++){
         if(vote[i] > popularest){
             popularest = vote[i]
             max = i + 1
         }
-         if(vote[i] < unpopularest){
-            unpopularest = vote[i]
-            min = i + 1
-        }
         }
         for(let i =0;i < vote.length;i++){
             totalVotes += vote[i]
         }
-        calculatePercent()
-        document.getElementById("totalVotes").textContent = "Total Votes: "+ totalVotes
-        document.getElementById("mostVote").textContent = "The most popular thing is: "+ max
-        document.getElementById("leastVote").textContent = "The most unpopular thing is: "+ min
-}
-function calculatePercent(){
-    let percentList = [0,0,0,0]
-    for(let i=0;i< vote.length; i++){
+
+        showResult(vote,totalVotes)
+})
+function showResult(List,sum){
+    if (sum != 0){
+        let percentList = [0,0,0,0]
+        for(let i=0;i < List.length; i++){
         percentList[i] = (
-            (vote[i]/totalVotes *100).toFixed(2)
+        (List[i]/sum *100).toFixed(2)
         )
     }
-    document.getElementById("percent1").textContent = "Option 1: "+percentList[0]+"%"
-    document.getElementById("percent2").textContent = "Option 2: "+percentList[1]+"%"
-    document.getElementById("percent3").textContent = "Option 3: "+percentList[2]+"%"
-    document.getElementById("percent4").textContent = "Option 4: "+percentList[3]+"%"
+    for(let i=0;i < percentList.length; i++){
+    document.getElementById('percent'+(i+1)).textContent = "Option "+ (i+1) +": "+List[i]+" votes"+" / "+percentList[i]+"%"
+    }
+        document.getElementById("totalVotes").textContent = "Total Votes: "+ sum
+        document.getElementById("mostVote").textContent = "The most popular thing is: "+ max
+        }else{
+        document.getElementById("percent1").textContent = "Option 1: --"
+        document.getElementById("percent2").textContent = "Option 2: --"
+        document.getElementById("percent3").textContent = "Option 3: --"
+        document.getElementById("percent4").textContent = "Option 4: --"
+        document.getElementById("totalVotes").textContent = "Total Votes: "
+        document.getElementById("mostVote").textContent = "The most popular thing is: "
+        }
 }
-function clear(){
+document.getElementById("clearBtn").addEventListener("click", function(){
     let checkboxes = [  
     document.getElementById("op1"),
     document.getElementById("op2"),
     document.getElementById("op3"),
     document.getElementById("op4")]
-    for(let i =0;i < checkboxes.length;i++){
+    for(let i = 0;i < checkboxes.length;i++){
         checkboxes[i].checked = false
     }
     vote = [0, 0, 0, 0]
     totalVotes = 0
-    term = 0
-    document.getElementById("percent1").textContent = "Option 1:--"
-    document.getElementById("percent2").textContent = "Option 2:--"
-    document.getElementById("percent3").textContent = "Option 3:--"
-    document.getElementById("percent4").textContent = "Option 4:--"
-    document.getElementById("totalVotes").textContent = "Total Votes: "
-    document.getElementById("mostVote").textContent = "The most popular thing is: "
-    document.getElementById("leastVote").textContent = "The most unpopular thing is: "
-}
+    max = 0
+    showResult(vote,totalVotes)
+})
