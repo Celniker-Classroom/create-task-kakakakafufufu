9c2f3fa193d27ec3235e5de9ae51b0829fba1a44
kakakakafufufu <chuir616@gmail.com>
2026-04-27 15:03:34 +0000

    Check all the bug and finish two main functions

--- script.js
+++ script.js
@@ -3,21 +3,33 @@ let vote = [0, 0, 0, 0]
 let totalVotes = 0
 let max = 0
 let options = ["Cat","Dog","Snake","Hamster"]
+let popularest = 0
+let time = 1
+document.getElementById("show").style.display = "none"
+document.getElementById("display").style.display = "none"
 
 let ask = prompt("What type of things do you want to investigate:")
 document.getElementById("remind").textContent = "Choose all the "+ask+" you like:"
+let term = 0
+for(let i =0;i < options.length;i++){
+    term = prompt("Pls input "+options.length+" options you want to investigate")
+    term = term.charAt(0).toUpperCase() + term.slice(1).toLowerCase();
+    options[i] = term
+}
+updatePage()
 
-
-    let term = 0
+document.getElementById("setBtn").addEventListener("click", function(){
+    resultStorage()
+    ask = prompt("What type of things do you want to investigate:")
+    document.getElementById("remind").textContent = "Choose all the "+ask+" you like:"
+    term = 0
     for(let i =0;i < options.length;i++){
         term = prompt("Pls input "+options.length+" options you want to investigate")
         term = term.charAt(0).toUpperCase() + term.slice(1).toLowerCase();
         options[i] = term
     }
-    document.getElementById("op2").textContent = options[1]
-    for(let i =0;i < option.length;i++){
-        document.getElementById('op'+(i+1)).textContent = options[i]
-    }
+    updatePage()
+})
 
 document.getElementById("submit").addEventListener("click", function(){
     document.getElementById("remind").textContent = "Choose all the "+ask+" you like:"
@@ -53,33 +65,27 @@ document.getElementById("submit").addEventListener("click", function(){
     }
         document.getElementById("totalVotes").textContent = "Total Votes: "+ totalVotes
         document.getElementById("mostVote").textContent = "The most popular thing is: "+ options[max-1]
-        document.getElementById("msg").textContent = "The most popular thing is: "+ options[max-1]
 
         }
     })
 
 document.getElementById("clearBtn").addEventListener("click", function(){
+    resultStorage()
     let checkboxes = [  
     document.getElementById("op1"),
     document.getElementById("op2"),
     document.getElementById("op3"),
     document.getElementById("op4")]
     for(let i = 0;i < checkboxes.length;i++){
-        checkboxes[i].checked = false
+        checkboxes[i].checked = false                  
     }
-    vote = [0, 0, 0, 0]
-    totalVotes = 0
-    max = 0
-    document.getElementById("percent1").textContent = "Cat: --"
-        document.getElementById("percent2").textContent = "Dog: --"
-        document.getElementById("percent3").textContent = "Snake: --"
-        document.getElementById("percent4").textContent = "Hamster: --"
+    updatePage()
         document.getElementById("totalVotes").textContent = "Total Votes: "
         document.getElementById("mostVote").textContent = "The most popular thing is: "
 })
 
 function findMax(voteList){
-    let popularest = 0
+    popularest = 0
     let max = 0
     for(let i =0;i < voteList.length;i++){
         if(voteList[i] > popularest){
@@ -88,4 +94,26 @@ function findMax(voteList){
         }
     }
     return max;
+}
+
+function updatePage(){
+    vote = [0, 0, 0, 0]
+    totalVotes = 0
+    max = 0
+    for(let i=0;i < options.length; i++){
+        document.getElementById("input"+(i+1)).textContent = options[i]
+        document.getElementById("percent"+(i+1)).textContent = options[i]+": --"
+    }
+}
+
+function resultStorage(){
+    if(totalVotes != 0){
+    let list = document.getElementById("record")
+    let li = document.createElement("li")
+    document.getElementById("display").style.display = "block"
+    document.getElementById("show").style.display = "block"
+    li.textContent = time+" round: "+options[max-1]+"   "+popularest+" votes"
+    list.appendChild(li)
+    time++
+    }
 }
